cf88753de304e135fbb3702120c56db105a0f149
Zubritckii Vladislav <vlad.zubritkij@yandex.ru>
2023-06-25 16:13:51 +0700

    Сделал задачу 13

--- Seminar 2/task 1/Program.cs
+++ Seminar 2/task 1/Program.cs
@@ -8,8 +8,8 @@ Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number >=100 && number <1000)
 {
-int mid = (number % 100 - number % 10) / 10;
-System.Console.WriteLine($"Вторая цифра числа {number} является {mid}");
+    int mid = (number % 100 - number % 10) / 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} является {mid}");
 }
 else
 System.Console.WriteLine("Это число не трехзначное");
