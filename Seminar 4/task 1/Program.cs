ad14bb40fcc6f3f17542281ba1de56d1fb80f06b
Zubritckii Vladislav <vlad.zubritkij@yandex.ru>
2023-07-01 21:28:02 +0700

    Сделал задачу 27

--- Seminar 4/task 1/Program.cs
+++ Seminar 4/task 1/Program.cs
@@ -1,9 +1,29 @@
-﻿
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
-int Number = 8;
-int[] array = new int[640000000];
-for (int i =1; i < array.Length; i++)
+
+
+int vvod (string message)
 {
-    array[i] = Random.Shared.Next(0,2);
-    Console.Write(array[i]);
+    Console.WriteLine("Введите число:");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
 }
+
+int SummaDigit(int number1)
+{
+    int result = 0;
+    while (number1 > 0)
+    {
+        result += number1 % 10;
+        number1 = number1 / 10;
+
+    }
+    return result;
+}
+
+int number = vvod ("Введите число: ");
+System.Console.WriteLine($"Сумма всех чисел в цифре {number}  = {SummaDigit(number)}");
+
