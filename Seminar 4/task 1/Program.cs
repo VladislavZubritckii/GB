41b259c54b863923eb917694f3bf15c9202468b7
Zubritckii Vladislav <vlad.zubritkij@yandex.ru>
2023-07-01 21:08:25 +0700

    Сделал задачу 25

---
+++ Seminar 4/task 1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+int Number = 8;
+int[] array = new int[640000000];
+for (int i =1; i < array.Length; i++)
+{
+    array[i] = Random.Shared.Next(0,2);
+    Console.Write(array[i]);
+}
---
+++ Seminar 4/task 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. НЕ ИСПОЛЬЗОВАТЬ MATH.POW()
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int vvod()
+{
+Console.WriteLine("Введите число:");
+int Number = Convert.ToInt32(Console.ReadLine());
+return Number;
+}
+int A = vvod();
+int B = vvod();
+int result = 1;
+for(int i = 1; i <= B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine(result);
+
+
+
+
+
